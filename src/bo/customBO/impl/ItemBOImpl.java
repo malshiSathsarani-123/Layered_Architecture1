3904f496c73c23e33cc75db5aebc473ace1a6952
malshiSathsarani-123 <malshisathsarani78@gmail.com>
2023-05-26 13:22:14 +0530

    ..

--- src/bo/customBO/impl/ItemBOImpl.java
+++ src/bo/customBO/impl/ItemBOImpl.java
@@ -1,47 +1,41 @@
-package bo;
+package bo.customBO.impl;
 
-import dao.custom.CustomerDAO;
+import bo.customBO.ItemBO;
 import dao.custom.ItemDAO;
-import dao.custom.impl.CustomerDAOImpl;
 import dao.custom.impl.ItemDAOImpl;
-import model.CustomerDTO;
 import model.ItemDTO;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class ItemBOImpl implements ItemBO {
+
+    ItemDAO itemDAO = new ItemDAOImpl();
+
     public ArrayList<ItemDTO> getAllItem() throws SQLException, ClassNotFoundException {
-        ItemDAO itemDAO = new ItemDAOImpl();
         return itemDAO.getAll();
     }
     public boolean addItem(ItemDTO dto) throws SQLException, ClassNotFoundException{
-        ItemDAO itemDAO = new ItemDAOImpl();
         return itemDAO.add(dto);
     }
 
     public boolean updateItem(ItemDTO dto) throws SQLException, ClassNotFoundException{
-        ItemDAO itemDAO = new ItemDAOImpl();
         return itemDAO.update(dto);
     }
 
     public boolean existItem(String id) throws SQLException, ClassNotFoundException {
-        ItemDAO itemDAO = new ItemDAOImpl();
         return itemDAO.exist(id);
     }
 
     public String generateNewID() throws SQLException, ClassNotFoundException {
-        ItemDAO itemDAO = new ItemDAOImpl();
         return itemDAO.generateNewID();
     }
 
     public boolean deleteItem(String id) throws SQLException, ClassNotFoundException{
-        ItemDAO itemDAO = new ItemDAOImpl();
         return itemDAO.delete(id);
     }
 
     public ItemDTO search(String id) throws SQLException, ClassNotFoundException {
-        ItemDAO itemDAO = new ItemDAOImpl();
         return itemDAO.search(id);
     }
 }
