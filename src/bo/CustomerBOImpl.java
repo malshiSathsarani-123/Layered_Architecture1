11599db85c1959b3aebd9a1ed8e307f4aac5a504
malshiSathsarani-123 <malshisathsarani78@gmail.com>
2023-05-26 13:11:58 +0530

    ..

--- src/bo/CustomerBOImpl.java
+++ src/bo/CustomerBOImpl.java
@@ -9,37 +9,32 @@ import java.util.ArrayList;
 
 public class CustomerBOImpl implements CustomerBO {
 
+    CustomerDAO customerDAO = new CustomerDAOImpl();
+
     public ArrayList<CustomerDTO> getAllCustomer() throws SQLException, ClassNotFoundException {
-        CustomerDAO customerDAO = new CustomerDAOImpl();
         return customerDAO.getAll();
     }
     public boolean addCustomer(CustomerDTO dto) throws SQLException, ClassNotFoundException{
-        CustomerDAO customerDAO = new CustomerDAOImpl();
         return customerDAO.add(dto);
     }
 
     public boolean updateCustomer(CustomerDTO dto) throws SQLException, ClassNotFoundException{
-        CustomerDAO customerDAO = new CustomerDAOImpl();
         return customerDAO.update(dto);
     }
 
     public boolean existCustomer(String id) throws SQLException, ClassNotFoundException {
-        CustomerDAO customerDAO = new CustomerDAOImpl();
         return customerDAO.exist(id);
     }
 
     public String generateNewID() throws SQLException, ClassNotFoundException {
-        CustomerDAO customerDAO = new CustomerDAOImpl();
         return customerDAO.generateNewID();
     }
 
     public boolean deleteCustomer(String id) throws SQLException, ClassNotFoundException{
-        CustomerDAO customerDAO = new CustomerDAOImpl();
         return customerDAO.delete(id);
     }
 
     public CustomerDTO search(String id) throws SQLException, ClassNotFoundException {
-        CustomerDAO customerDAO = new CustomerDAOImpl();
         return customerDAO.search(id);
     }
 }
